92dd1a080917a3e9836b7f5b37349e2deff3b331
Infamou5 <oreal_b52@hotmail.com>
2021-06-29 19:02:42 +0200

    Completed the fist task of the homework.( Flight booking fullname function ). Tested it with the codepen form that was given.

---
+++ javascript/javascript1/week2/index.js
@@ -0,0 +1,34 @@
+/*________________________________________*/
+/*  HYF HOMEWORK - JAVASCRIPT 1 - WEEK 2  */
+/*________________________________________*/
+
+/* TASK 1 - Flight booking fullname function */
+
+function getFullname(firstname, surname, useFormalName) {
+    if (!firstname || !surname) throw new Error('Whoopsy, Firstname or Surename is missing.');
+    return ((useFormalName && useFormalName === true ? `${"Lord " + firstname + " " + surname}` : `${firstname + " " + surname}`));
+}
+
+getFullname("Benjamin", "Hughes", true);
+
+/* ANSWER for the QUESTION : But what if the person is a woman? Describe how you would fix the getFullname so it also works for women */
+/* 1. I'd create another param something like isMale.
+   2. I'd create a string variable called formalName.
+   3. Depending on the value of the isMale boolean I would change the formaleName's string value from "Lord"  TO  "Lady".
+
+    It would look something like this:
+
+    function getFullname(firstname, surname, useFormalName, isMale) {
+        let formaleName = "Lord";  IT WOULD BE THE DEFAULT VALUE that I initialize the variable with.
+        if (isMale && isMale === false){               IF isMale boolean is given and it's value is false,change formalName's value to "Lady".
+        formalName = "Lady";
+        }
+
+        if (!firstname || !surname) throw new Error('Whoopsy, Firstname or Surename is missing.');
+        return ((useFormalName && useFormalName === true ? `${ formalName + " " + firstname + " " + surname}` : `${firstname + " " + surname}`));
+}
+
+getFullname("Benjamin", "Hughes", true, false); -> would return: Lady Benjamin Hughes as getFullname's value.
+
+/*================================================================*/
+
