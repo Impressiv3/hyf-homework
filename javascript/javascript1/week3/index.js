feeb43b709cd3677f93e167a3d3b3ad2f6cc3897
Infamou5 <oreal_b52@hotmail.com>
2021-08-01 12:33:44 +0200

    Implemented TASK3 - Series duration of my life

--- javascript/javascript1/week3/index.js
+++ javascript/javascript1/week3/index.js
@@ -21,6 +21,7 @@ console.table(names);
 
 /* =========================================== */
 
+
 /* TASK 2 - When will we be there?? */
 
 const travelInformation = {
@@ -39,3 +40,51 @@ function timeTillDestination() {
 console.log(travelTime)
 
 /* =========================================== */
+
+/* TASK 3 - Series duration of my life */
+
+const seriesDurations = [
+    {
+        title: "Money heist",
+        days: 3,
+        hours: 1,
+        minutes: 0,
+    },
+    {
+        title: "Defiance",
+        days: 3,
+        hours: 21,
+        minutes: 14,
+    },
+    {
+        title: "Breaking bad",
+        days: 3,
+        hours: 11,
+        minutes: 20,
+    },
+];
+
+const averageLifespan = 80;
+const averageLifespanMinutes = averageLifespan * 365 * 24 * 60; /* Convert years to minutes */
+let percentageOfLife = 0;
+let totalPercentageOfLife = 0;
+
+function logOutSeriesText(seriesDurations) {
+    let serieLengthInMinutes = 0;
+
+    for (let i = 0; i < seriesDurations.length; i++) {
+        serieLengthInMinutes = (seriesDurations[i].days * 1440) + (seriesDurations[i].hours * 60) + seriesDurations[i].minutes;
+        percentageOfLife = ((serieLengthInMinutes / averageLifespanMinutes) * 100);
+        totalPercentageOfLife += percentageOfLife;
+        console.log(seriesDurations[i].title + " took " + percentageOfLife.toFixed(3) + "% of my life.");
+    }
+
+    console.log("In total that is " + totalPercentageOfLife.toFixed(3) + "% of my life.");
+}
+
+logOutSeriesText(seriesDurations);
+/* logOutSeriesText(); // logs out the text found above */
+
+/* =========================================== */
+
+
