e9a26baf2a6811330a1c65557621b9261be1eb58
Infamou5 <oreal_b52@hotmail.com>
2021-08-09 16:01:49 +0200

    [ TASK 1. - Find the shortest word ] - COMLETED
    Have implemented the first task of the homework.

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -1,4 +1,45 @@
-console.log("Script loaded");
+/*
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+██░██░██░███░██░▄▄▄████░██░██░▄▄▄░██░▄▀▄░██░▄▄▄██░███░██░▄▄▄░██░▄▄▀██░█▀▄
+██░▄▄░██▄▀▀▀▄██░▄▄█████░▄▄░██░███░██░█░█░██░▄▄▄██░█░█░██░███░██░▀▀▄██░▄▀█
+██░██░████░████░███████░██░██░▀▀▀░██░███░██░▀▀▀██▄▀▄▀▄██░▀▀▀░██░██░██░██░
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+_________________________ 
+|  _____________________  |
+| | JAVASCRIPT2 - WEEK1 | |
+| |_____________________| |
+|_________________________|                                              */
+
+/*================[ TASK 1. - Find the shortest word ]================== */
+
+const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium'];
+let shortestWord = '';
+let initialShortestWordLength = 0;
+function findShortestWord(wordsToCheck) {
+  shortestWord = wordsToCheck.reduce((currentWord, shortest) => {
+    if (currentWord.length < shortest.length) {
+      return currentWord;
+    }
+    return shortest;
+  }, initialShortestWordLength);
+}
+
+findShortestWord(danishWords);
+console.log(shortestWord);
+
+/*=======================================================================*/
+
+/* console.log("Script loaded");
 
 const products = getAvailableProducts();
-console.log(products);
+console.log(products); */
+/* 
+let array = [1, 2, 3, 4, 5];
+let initialMax = 0;
+const max = array.reduce((element, max) => {
+  if (element > max) {
+    return element;
+  }
+  return max;
+}, initialMax);
+max; // 5 */
