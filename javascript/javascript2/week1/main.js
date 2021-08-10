44d98d85f0ab8a75f43d3c014cc5c3875dec54da
Infamou5 <oreal_b52@hotmail.com>
2021-08-10 19:48:03 +0200

    [ TASK 3. - Spirit animal name generator ] - COMPLETED
    Have implemented TASK3.
    Updated background image and did some small .css adjustments.

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -9,9 +9,9 @@ _________________________
 | | JAVASCRIPT2 - WEEK1 | |
 | |_____________________| |
 |_________________________|                                              */
-
+console.log("====================[  Script loaded   ]====================");
+//#region TASK 1 - START
 /*================[ TASK 1. - Find the shortest word ]================== */
-
 // Write a function that finds the shortest word of an array of words.
 
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
@@ -30,7 +30,9 @@ findShortestWord(danishWords);
 console.log(`The sortest word is : ${shortestWord}`);
 
 /*=======================================================================*/
+//#endregion TASK 1 - END
 
+//#region TASK 2 - START
 /*===========[ TASK 2. - Find and count the Danish letters ]============ */
 
 // Find the individual and the total number of Danish letters in a string.
@@ -67,18 +69,85 @@ danishLetterCounter(danishString1);
 danishLetterCounter(danishString2);
 
 /*=======================================================================*/
+//#endregion TASK 2 - END
 
-/* console.log("Script loaded");
+//#region TASK 3 - START
+/*=============[ TASK 3. - Spirit animal name generator ]=============== */
+
+////////////////////////// VARIABLES  ////////////////////////////////////
+
+let userName = "";
+const firstAnimalName = ["The sick", "The bored", "The fantastic", "The wild"];
+const secondAnimalName = ["Wolf", "Caterpillar", "Alpaca", "Sloth"];
+const spiritAnimalNameOutput = document.querySelector(
+  "#spiritAnimalNameOutput"
+);
+
+const submitButton = document.querySelector("#submit");
+const realName = document.querySelector("#realName");
+const radioButton1 = document.querySelector("#radio1");
+const radioButton2 = document.querySelector("#radio2");
+const radioButton3 = document.querySelector("#radio3");
+
+//////////////////// EVENT LISTENERS  ////////////////////////////////////
+
+radioButton1.addEventListener("change", addOnClickEventListenerToElement);
+radioButton2.addEventListener("change", addOnMouseEnterEventListenerToElement);
+radioButton3.addEventListener("change", addOnInputEventListenerToElement);
+
+//////////////////// UTILITY FUNCTIONS  //////////////////////////////////
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomNames() {
+  return `${firstAnimalName[randomNumber(1, firstAnimalName.length - 1)]} ${
+    secondAnimalName[randomNumber(1, secondAnimalName.length - 1)]
+  }`;
+}
+
+function showAnimalName(userName) {
+  return userName + " : " + `${getRandomNames()}`;
+}
+
+function addOnClickEventListenerToElement() {
+  submitButton.addEventListener("click", submitForm);
+  realName.removeEventListener("mouseenter", submitForm);
+  realName.removeEventListener("input", submitForm);
+}
+
+function addOnMouseEnterEventListenerToElement() {
+  submitButton.addEventListener("click", submitForm);
+  realName.addEventListener("mouseenter", submitForm);
+  realName.removeEventListener("input", submitForm);
+}
+
+function addOnInputEventListenerToElement() {
+  submitButton.removeEventListener("click", submitForm);
+  realName.removeEventListener("mouseenter", submitForm);
+  realName.addEventListener("input", submitForm);
+}
+
+function submitForm() {
+  userName = realName.value;
+  !userName
+    ? (spiritAnimalNameOutput.innerHTML = "Please enter a name first...")
+    : (spiritAnimalNameOutput.innerHTML = showAnimalName(userName));
+}
+
+addOnClickEventListenerToElement();
+
+/*=======================================================================*/
+//#endregion TASK 3 - END
+
+//#region TASK 4 - START
+/*========================[ TASK 4. - HYFBay ]========================== */
+
 
 const products = getAvailableProducts();
-console.log(products); */
-/* 
-let array = [1, 2, 3, 4, 5];
-let initialMax = 0;
-const max = array.reduce((element, max) => {
-  if (element > max) {
-    return element;
-  }
-  return max;
-}, initialMax);
-max; // 5 */
+console.log(products);
+
+/*=======================================================================*/
+//#endregion TASK 4 - END
+
