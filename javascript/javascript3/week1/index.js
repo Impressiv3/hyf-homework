b1269c73745c23818cc66c9da437b7e456910f84
Infamou5 <oreal_b52@hotmail.com>
2021-08-27 17:20:03 +0200

    [ TASK 1 ] Create own json file [ COMPLETED ]

--- javascript/javascript3/week1/index.js
+++ javascript/javascript3/week1/index.js
@@ -6,6 +6,8 @@
 
 //      JAVASCRIPT 3 - WEEK 1    //
 
-//#region START------------[ TASK 1 ] Warmup------------------
+//#region START----------[ TASK 1 ] Create own json file----------------
 
-//#endregion END-----------[ TASK 1 ] Warmup------------------
+// own JSON file in the root directory called myOwn.json
+
+//#endregion END---------[ TASK 1 ] Create own json file----------------
