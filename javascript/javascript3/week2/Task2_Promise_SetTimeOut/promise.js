da5ed90f1931cc6dba6613d179813e01bb865ac5
Infamou5 <oreal_b52@hotmail.com>
2021-09-11 13:27:16 +0200

    Completed TASK 5 - Visual Promise

--- javascript/javascript3/week2/Task2_Promise_SetTimeOut/promise.js
+++ javascript/javascript3/week2/Task2_Promise_SetTimeOut/promise.js
@@ -1,6 +1,7 @@
 // TASK 2 Promise that resolves after set time
 
-const ownFunction = (delay) => new Promise((resolve) => setTimeout(resolve, delay * 1000));
+const ownFunction = (resolveAfter) => {
+  return new Promise((resolve, reject) => setTimeout(resolve, resolveAfter * 1000));
+};
 ownFunction(3).then(() => console.log("I am called asynchronously"));
-
 ///////////////////////////////////////////////////////////////////////////////
--- javascript/javascript3/week2/Task5_Visual_Promise/main.js
+++ javascript/javascript3/week2/Task5_Visual_Promise/main.js
@@ -2,12 +2,50 @@ const redBox = document.querySelector("ul.marks li:nth-child(1)");
 const blueBox = document.querySelector("ul.marks li:nth-child(2)");
 const greenBox = document.querySelector("ul.marks li:nth-child(3)");
 const boxes = [redBox, blueBox, greenBox];
+const redTarget = { color: "red", x: 20, y: 300 };
+const blueTarget = { color: "blue", x: 400, y: 300 };
+const greenTarget = { color: "green", x: 400, y: 20 };
+const redPosition = {
+  x: `${redTarget.x - parseInt(redBox.style.left)}`,
+  y: `${redTarget.y - parseInt(redBox.style.top)}`,
+};
+const bluePosition = {
+  x: `${blueTarget.x - parseInt(blueBox.style.left)}`,
+  y: `${blueTarget.y - parseInt(blueBox.style.top)}`,
+};
+const greenPosition = {
+  x: `${greenTarget.x - parseInt(greenBox.style.left)}`,
+  y: `${greenTarget.y - parseInt(greenBox.style.top)}`,
+};
 
 async function translateOneByOne() {
-  boxes.forEach((box, index) => {
-      
-    console.log(`${index} : ${box}`);
+  await moveElement(redBox, redPosition).then(() => {
+    console.log("Red has been moved.");
+  });
+
+  await moveElement(blueBox, bluePosition).then(() => {
+    console.log("Blue has been moved.");
+  });
+
+  await moveElement(greenBox, greenPosition).then(() => {
+    console.log("Green has been moved.");
   });
 }
 
-translateOneByOne();
+/* translateOneByOne(); */
+
+function translateAllAtOnce() {
+  Promise.all([
+    moveElement(redBox, redPosition),
+    moveElement(blueBox, bluePosition),
+    moveElement(greenBox, greenPosition),
+  ])
+    .then(() => {
+      console.log("All circles/boxes moved.");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+translateAllAtOnce();
