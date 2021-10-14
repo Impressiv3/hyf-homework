f7224354b8aa12b535e86af3e812c7148a980757
Bálint Szabad <oreal_b52@hotmail.com>
2021-10-14 17:22:24 +0200

    Added new meals, reservations and reviews

--- nodejs/week1/homework/src/backend/app.js
+++ nodejs/week1/homework/src/backend/app.js
@@ -3,10 +3,22 @@ const app = express();
 
 // import data here
 const meals = require("./data/meals");
+const reviews = require("./data/reviews");
+const reservations = require("./data/reservations");
+
+const getMeals = () => {
+  console.log("Yello world!");
+};
 
 // this is where you will be adding your routes
 app.get("/", async (request, response) => {
-  response.send("asd");
+  response.statusCode = 200;
+  response.send("Changed the default message!");
+});
+
+app.get("/meals", async (request, response) => {
+  response.statusCode = 200;
+  response.send("Lofaszz");
 });
 
 module.exports = app;
--- nodejs/week1/homework/warmup.js
+++ nodejs/week1/homework/warmup.js
@@ -1,1 +1,36 @@
 console.log("inside warmup file");
+console.log(" inside warmup file ");
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  getDiameter() {
+    return this.radius * 2;
+  }
+
+  getCircumference() {
+    return this.radius * 2 * Math.PI;
+  }
+
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+const circle1 = new Circle(10);
+const circle2 = new Circle(20);
+const circle3 = new Circle(30);
+
+console.log(circle1.getDiameter());
+console.log(circle1.getCircumference());
+console.log(circle1.getArea());
+
+console.log(circle2.getDiameter());
+console.log(circle2.getCircumference());
+console.log(circle2.getArea());
+
+console.log(circle3.getDiameter());
+console.log(circle3.getCircumference());
+console.log(circle3.getArea());
