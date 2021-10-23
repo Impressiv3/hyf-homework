4c1ee0fe9553dc989d086f14803f9647bad08689
Bálint Szabad <oreal_b52@hotmail.com>
2021-10-23 12:12:58 +0200

    Implemented the 4 basic queries without error handling and  edge cases

--- nodejs/week2/homework/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/homework/meal-sharing/src/backend/api/meals-router.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const meals = require("./../data/meals.json");
 
 /* Respond with the json for all the meals */
-router.get("/", async (req, res, next) => {
+/* router.get("/", async (req, res, next) => {
   try {
     res.json(meals);
-    next();
+    next()
   } catch (error) {
     throw error;
   }
-});
+});  */
 
 /* Respond with the json for the meal with the corresponding id */
 router.get("/:id", async (req, res, next) => {
@@ -26,5 +26,32 @@ const getMeal = (id) => {
   return meals.filter((meal) => meal.id === id);
 };
 
+router.get("/", async (req, res) => {
+  const { maxPrice, title, createdAfter, limit } = req.query;
+  let filteredMeals = meals;
+  if (Object.keys(req.query).length === 0) {
+  } else {
+    if (maxPrice) {
+      filteredMeals = filteredMeals.filter((filteredMeal) => filteredMeal.price <= maxPrice);
+    } else {
+    }
+    if (title) {
+      filteredMeals = filteredMeals.filter((filteredMeal) =>
+        filteredMeal.title.toLowerCase().includes(title),
+      );
+    } else {
+    }
+    if (createdAfter) {
+      filteredMeals = filteredMeals.filter(
+        (filteredMeal) => new Date(filteredMeal.createdAt) > new Date(createdAfter),
+      );
+    } else {
+    }
+    if (limit) {
+      filteredMeals = filteredMeals.slice(0, parseInt(limit));
+    }
+  }
+  res.json(filteredMeals);
+});
 
 module.exports = router;
