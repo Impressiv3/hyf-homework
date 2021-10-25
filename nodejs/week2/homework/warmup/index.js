12305d737d4afe30c3b717fb666d2feba78b5c5c
Infamou5 <oreal_b52@hotmail.com>
2021-10-25 17:57:54 +0200

    Deleted node week2

--- nodejs/week2/homework/warmup/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const router = require("./router.js");
-
-app.use("/", router);
-
-app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
